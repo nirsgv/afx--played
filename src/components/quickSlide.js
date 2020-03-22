745a583f4a74c9bf067e952ba51adfd79a4d2def
Nir Segev <nirsegevmail@gmail.com>
2020-03-22 19:22:11 +0200

    buttons toggled by need

--- src/components/quickSlide.js
+++ src/components/quickSlide.js
@@ -2,11 +2,11 @@ import React, { useState, useRef, useEffect } from 'react';
 import Icon from './icon';
 
 function QuickSlide ({children})  {
-    const [translatedX, setTranslatedX] = useState(0);
-    const [slidesWidth, setSlidesWidth] = useState(0);
-    const [wrapperWidth, SetWrapperWidth] = useState(0);
-    const ref = useRef(null);
-    const ref2= useRef(null);
+    const [translatedX, setTranslatedX] = useState(0),
+          [slidesWidth, setSlidesWidth] = useState(0),
+          [wrapperWidth, SetWrapperWidth] = useState(0),
+          ref = useRef(null),
+          ref2= useRef(null);
     useEffect(() => {
         const width = ref.current ? ref.current.offsetWidth : 0;
         const wrapperW = ref2.current ? ref2.current.offsetWidth : 0;
@@ -14,24 +14,33 @@ function QuickSlide ({children})  {
         SetWrapperWidth(wrapperW);
     }, [ref.current, ref2.current]);
 
+
+
+    const isLeftScrollNeeded = (translatedX) => {
+        return translatedX < 0;
+    };
+
+    const isRightScrollNeeded = (translatedX) => {
+        return wrapperWidth - translatedX < slidesWidth;
+    };
+
     const clickedLeft = (translatedX) => {
-        if (translatedX < 0 )setTranslatedX(translatedX + 200);
+        isLeftScrollNeeded(translatedX) &&  setTranslatedX(translatedX + 200);
     };
 
     const clickedRight = (translatedX) => {
-        console.log({ translatedX, slidesWidth });
-        if (wrapperWidth - translatedX < slidesWidth ) setTranslatedX(translatedX - 200);
+        isRightScrollNeeded(translatedX) &&  setTranslatedX(translatedX - 200);
     };
 
     return (
         <div className="quick-slide__wrap" ref={ref2}>
-            <button className="quick-slide__button quick-slide__button--left" onClick={()=>clickedLeft(translatedX)}>left</button>
+            {isLeftScrollNeeded(translatedX) && <button className="quick-slide__button quick-slide__button--left" onClick={()=>clickedLeft(translatedX)}>left</button>}
             <div className="quick-slide__content" style={{transform:`translateX(${translatedX}px)`}} >
                 <ul className="quick-slide__list" ref={ref}>
                     {children}
                 </ul>
             </div>
-            <button className="quick-slide__button quick-slide__button--right" onClick={()=>clickedRight(translatedX)}>right</button>
+            {isRightScrollNeeded(translatedX) && <button className="quick-slide__button quick-slide__button--right" onClick={()=>clickedRight(translatedX)}>right</button>}
         </div>
     )
 };
