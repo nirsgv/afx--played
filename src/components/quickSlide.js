62eb2cb946c2162463bfc2fd8a04cee8e56613dd
Nir Segev <nirsegevmail@gmail.com>
2020-03-22 20:17:03 +0200

    buttons svg icons implemented

--- src/components/quickSlide.js
+++ src/components/quickSlide.js
@@ -1,5 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Icon from './icon';
+import SvgSprite from "./svgSprite";
+import {imgData} from "../data/localImgData";
+import InputBox from "./inputBox";
 
 function QuickSlide ({children})  {
     const [translatedX, setTranslatedX] = useState(0),
@@ -34,15 +37,26 @@ function QuickSlide ({children})  {
 
     return (
         <div className="quick-slide__wrap" ref={ref2}>
-            <button className={`quick-slide__button quick-slide__button--left quick-slide__button--${isLeftScrollNeeded(translatedX) ? 'active' : 'disabled'}`}
-                    onClick={()=>clickedLeft(translatedX)}>left</button>
-            <div className="quick-slide__content" style={{transform:`translateX(${translatedX}px)`}} >
+
+            <button
+                className={`quick-slide__button quick-slide__button--left quick-slide__button--${isLeftScrollNeeded(translatedX) ? 'active' : 'disabled'}`}
+                onClick={() => clickedLeft(translatedX)}>
+                <SvgSprite classes={'quick-slide__icon quick-slide__icon--left'} src={imgData.sprite.src}
+                           alt={imgData.sprite.description} name={'ANGLE_LEFT'}/>
+            </button>
+
+            <div className="quick-slide__content" style={{transform: `translateX(${translatedX}px)`}}>
                 <ul className="quick-slide__list" ref={ref}>
                     {children}
                 </ul>
             </div>
-            <button className={`quick-slide__button quick-slide__button--right quick-slide__button--${isRightScrollNeeded(translatedX) ? 'active' : 'disabled'}`}
-                    onClick={()=>clickedRight(translatedX)}>right</button>
+
+            <button
+                className={`quick-slide__button quick-slide__button--right quick-slide__button--${isRightScrollNeeded(translatedX) ? 'active' : 'disabled'}`}
+                onClick={() => clickedRight(translatedX)}>
+                <SvgSprite classes={'quick-slide__icon quick-slide__icon--right'} src={imgData.sprite.src}
+                           alt={imgData.sprite.description} name={'ANGLE_RIGHT'}/>
+            </button>
         </div>
     )
 };
