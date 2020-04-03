22e216dc7b6420dc9619850271b6e33fad71a86f
Nir Segev <nirsegevmail@gmail.com>
2020-04-03 14:02:27 +0300

    added a footer stylesheet

--- src/components/items.js
+++ src/components/items.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Item from "./item";
 
 
-class Items extends React.Component {
+function Items ({ tracksFiltered, isPlayingEmbedded, setPlayerItem }) {
 
-    shouldComponentUpdate(nextProps, nextState) {
-        // do not rerender unless this specific property changed
-        return nextProps.tracksFiltered !== this.props.tracksFiltered;
-    }
-
-    render(props) {
-        //console.log(this.props.tracksFiltered);
-        return (
-            <>
-                {this.props.tracksFiltered.map((item, index) => {
-                    return (
-                        <Item key={index} trackData={item} isPlayingEmbedded={this.props.isPlayingEmbedded} setPlayerItem={this.props.setPlayerItem}/>
-                    )
-                })}
-            </>
-        )
-    }
+    return (
+        <>
+            {tracksFiltered.map((item, index) => {
+                return (
+                    <Item key={index} trackData={item} isPlayingEmbedded={isPlayingEmbedded} setPlayerItem={setPlayerItem}/>
+                )
+            })}
+        </>
+    )
 }
+
+// class Items extends React.Component {
+//
+//     shouldComponentUpdate(nextProps, nextState) {
+//         // do not rerender unless this specific property changed
+//         return nextProps.tracksFiltered !== this.props.tracksFiltered;
+//     }
+//
+//     render(props) {
+//         //console.log(this.props.tracksFiltered);
+//         return (
+//             <>
+//                 {this.props.tracksFiltered.map((item, index) => {
+//                     return (
+//                         <Item key={index} trackData={item} isPlayingEmbedded={this.props.isPlayingEmbedded} setPlayerItem={this.props.setPlayerItem}/>
+//                     )
+//                 })}
+//             </>
+//         )
+//     }
+// }
 
 export default Items;
