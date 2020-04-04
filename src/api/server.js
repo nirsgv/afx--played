981ccb12be7e4c3b1e3b1cdc07dae973976d79c4
Nir Segev <nirsegevmail@gmail.com>
2020-04-04 20:12:12 +0300

    changes

--- src/api/server.js
+++ src/api/server.js
@@ -12,7 +12,7 @@ const SHOWS = require('../data/showsMap');
 const app = express();
 
 const normalizePort = port => parseInt(port, 10);
-const PORT = normalizePort(process.env.PORT || 3000);
+const PORT = normalizePort(process.env.PORT || 5000);
 
 const dev = app.get('env') !== 'production';
 
@@ -21,7 +21,7 @@ if (!dev) {
     app.use(compression());
     app.use(morgan('common'));
     app.use(express.static(path.resolve(__dirname, 'build')));
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 } else {
