66b256d52ed542cf917dd9f36f535063fc57dcc7
Nir Segev <nirsegevmail@gmail.com>
2020-04-05 17:18:25 +0300

    footer fetching animation on homepage is setup, some cleanup is needed

---
+++ src/components/animativeIndicator.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import SvgSprite from "./svgSprite";
+import { imgData } from '../data/localImgData';
+
+
+function AnimativeIndicator({ animateFooter, setAnimateFooter, pageName }) {
+
+
+    return (
+        <>
+            <section className={`footer__preloader ${animateFooter ? 'footer__preloader--animate-fetch' : ''}`}
+                     data-test="splash-root"
+                     onAnimationEnd={() => setAnimateFooter(false)}
+            >
+                <div className='footer__animative-indicator'></div>
+            </section>
+        </>
+    );
+}
+
+export default AnimativeIndicator;
