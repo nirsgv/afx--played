51519eb6d4bf660e5ee5eb0cf1dbb4cd2fd64a75
Nir Segev <nirsegevmail@gmail.com>
2020-04-06 00:38:46 +0300

    pkg json

---
+++ src/credentials.js
@@ -0,0 +1,8 @@
+const auth = {
+    auth: {
+        api_key: 'key-4ef4d519bb6ed0a5c84ff787b8c58b95',
+        domain: 'sandbox75d7bb2aedca490b872210868695893c.mailgun.org'
+    }
+};
+// 1c059c03ff7a59e5035759701bd23417-ee13fadb-b4c35f1a
+module.exports = auth;
