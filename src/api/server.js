d00775066493c919b32668e80093e4569940e09b
Nir Segev <nirsegevmail@gmail.com>
2020-04-05 23:18:59 +0300

    5000 stable

--- src/api/server.js
+++ src/api/server.js
@@ -16,17 +16,15 @@ const PORT = normalizePort(process.env.PORT || 5000);
 
 const dev = app.get('env') !== 'production';
 
-if (!dev) {
-    app.disable('x-powered-by');
-    app.use(compression());
-    app.use(morgan('common'));
-    app.use(express.static(path.resolve(__dirname, 'build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-} else {
-    app.use(morgan('dev'));
-}
+app.disable('x-powered-by');
+app.use(compression());
+app.use(morgan('common'));
+app.use(express.static(__dirname + '/../../build'));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(express.static(__dirname, '/../../build', 'index.html'));
+// });
+app.use(morgan('dev'));
 
 const server = createServer(app);
 console.log(process.env.NODE_ENV);
