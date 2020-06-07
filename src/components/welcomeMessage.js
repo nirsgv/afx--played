826e490c8ccec4d4f82aa594e017c4d27ec3a3cd
nirsgv <nirsegevmail@gmail.com>
2020-06-07 18:51:36 +0300

    intro component

--- src/components/welcomeMessage.js
+++ src/components/welcomeMessage.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SvgSprite from "./svgSprite";
 
 function BackButton({ ...restProps }) {
     const { className = 'welcome-message', history, cancelWelcomeIntro } = restProps;
 
-    const waitLittle = () => {
-      setTimeout(cancelWelcomeIntro, 1000);
-    };
 
 
     return (
-        <aside className={className} onClick={waitLittle}>
-            <h1 className={`${className}__headline`}>Hello!</h1>
-            <p className={`${className}__paragraph`}>
-                This site presents <strong>a list</strong> of tracks.<br />
-                These tracks were played by Richard James aka The Aphex Twin during his shows.
-            </p>
-            <Link className={`${className}__link`} to="/about">More info</Link>
+        <aside className={className} onClick={cancelWelcomeIntro}>
+            <div className={`${className}__content`}>
+                <h1 className={`${className}__headline`}>Hello!</h1>
+                <p className={`${className}__paragraph`}>
+                    This site presents <strong>a list</strong> of tracks.<br />
+                    These tracks were played by Richard James aka The Aphex Twin during his shows.
+                </p>
+                <Link className={`${className}__link`} to="/about">More info</Link>
+
+            </div>
+            <div className={`${className}__close-wrap`}>
+                <button onClick={cancelWelcomeIntro} className={`${className}__close-button`}>
+                    <SvgSprite name={'TIMES'} />
+                </button>
+            </div>
 
         </aside>
     );
