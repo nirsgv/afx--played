b21479b3806f60ea31b26684d45d0c307063be8d
nirsgv <nirsegevmail@gmail.com>
2020-06-08 10:58:26 +0300

    about map styling json

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,25 @@
 import React, { useEffect } from 'react';
 import { Helmet } from "react-helmet";
 import BackButton from "./backButton";
+import ShowGoogleMap from "./showsGoogleMap";
+
+
+const AnyReactComponent = ({ lat, lng, text }) => {
+    return <h3>{`${lat}${lng}${text}`}</h3>
+};
+
+const handleApiLoaded = (map, maps) => {
+    // use map and maps objects
+};
+
+const defaultMapProps = {
+    center: {lat: 40.73, lng: -73.93},
+    zoom: 12,
+    bootstrapURLKeys: { key: 'AIzaSyCKj8YlOvnus07jxLryEGnHrITN2z8a0_I' },
+    yesIWantToUseGoogleMapApiInternals: true,
+    onGoogleApiLoaded: ({ map, maps }) => handleApiLoaded(map, maps),
+};
+
 function About(props) {
 
     useEffect(() => {
@@ -11,6 +30,7 @@ function About(props) {
 
 
     return (
+
         <>
             <Helmet>
                 <title>About page...</title>
@@ -19,6 +39,8 @@ function About(props) {
             <BackButton history={props.history} className={"back-btn"}/>
 
             <h1>About, {props.name}</h1>
+asdasd
+            <ShowGoogleMap />
 
             <form>
                 <label>
