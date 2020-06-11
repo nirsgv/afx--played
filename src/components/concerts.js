22db7ec86d1bd8be2f3f1f3fed4fc1e9fd139a3e
nirsgv <nirsegevmail@gmail.com>
2020-06-11 17:36:03 +0300

    fix concerts 2

--- src/components/concerts.js
+++ src/components/concerts.js
@@ -1,17 +1,17 @@
 import React from "react";
-import concertsData  from "../data/shows";
 import List  from "../components/list";
 import { Link } from "react-router-dom";
 import SvgSprite from "./svgSprite";
 import PropTypes from 'prop-types';
 
-const shows = JSON.parse(localStorage.getItem("afx_local_shows")).data;
-const getConcertById = (id, concertsData) => {
+const shows = JSON.parse(localStorage.getItem("afx_local_shows")),
+      showsData = shows ? shows.data : false,
+      getConcertById = (id, concertsData) => {
     return concertsData.find(concert => concert.SHOW_ID === id);
 };
 
 function Concerts({ venues }) {
-    const concertsCollection = venues.map(item => getConcertById(item, shows));
+    const concertsCollection = shows && venues.map(item => getConcertById(item, showsData));
     console.error(concertsCollection);
     {return concertsCollection.filter(item => !!item).length ?
      (
